13ac2c898e96608bff6e89e7d4aa90e92f1fee5a
Narayan Sharma <narayansharma@ucreate.co.in>
2019-01-10 13:07:34 +0530

    debuging

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -15,6 +15,21 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        phpinfo();
+        //\Artisan::call('migrate');
+        try {
+            $db = new \PDO('pgsql:host=postgres-test;port=5432;dbname=test;user=postgres;password=postgres');
+
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $sql ='CREATE TABLE IF NOT EXISTS test1 (id serial PRIMARY KEY, prename VARCHAR(50) NOT NULL);';
+            $db->exec($sql);
+            echo 'success';
+        } catch (\Exception $e) {
+            dd($e->getMessage());
+        }
+
+
         $this->assertTrue(true);
     }
 }
