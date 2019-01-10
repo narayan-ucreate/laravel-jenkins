4fd4656d04cb4fd7266ac7a91a959371c3a706f3
Narayan Sharma <narayansharma@ucreate.co.in>
2019-01-10 16:31:05 +0530

    add static seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,6 +13,15 @@ class UserSeeder extends Seeder
     public function run()
     {
         \DB::table('users')->truncate();
-        factory(App\User::class, 10)->create();
+        \DB::table('users')->insert(
+          [
+              [
+                  'name' => 'Narayan Sharma',
+                  'email' => 'email@gmail.com',
+                  'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+                  'remember_token' => str_random(10),
+              ]
+          ]
+        );
     }
 }
